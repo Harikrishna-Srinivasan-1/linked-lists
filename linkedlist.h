feedbe7a8045811d81c01a01239703fde1e9ad69
Harikrishna-Srinivasan-1 <harikrishnasri3@gmail.com>
2024-08-09 18:14:43 +0530

    Create linkedlist.h
    Abstract class to be inherited by other classes.

---
+++ linkedlist.h
@@ -0,0 +1,74 @@
+#include <iostream>
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+const size_t NOT_FOUND = -1;
+
+template <typename A>;
+class linkedlist
+{
+    protected:
+        typedef struct node
+        {
+            A value;
+            struct node *next;
+        } node;
+
+        node *head;
+        int length;
+    public:
+        linkedlist();
+
+        virtual void prepend(const A &value) = 0;
+        virtual void append(const A &value) = 0;
+        virtual void insert(const A &value, const size_t &index) = 0;
+        virtual void insert_inorder(const A &value) = 0;
+        virtual void extend(const linkedlist<A> &obj) = 0;
+
+        virtual void remove(const A &value) = 0;
+        virtual A pop(const size_t &index) = 0;
+        virtual A pop() = 0;
+
+        virtual size_t size() const;
+
+        virtual size_t find(const A &value) const = 0;
+        virtual size_t count(const A &value) const = 0;
+
+        virtual bool isempty() const;
+
+        virtual A &operator[](const size_t &index) = 0;
+        virtual linkedlist<A> operator+(linkedlist<A> &obj) = 0;
+
+        virtual void freed() = 0;
+};
+
+size_t len(const linkedlist &obj);
+
+class Exception
+{
+    protected:
+        std::string what_arg;
+    public:
+        Exception();
+        std::string what_err() const;
+};
+
+class IndexError: public Exception
+{
+    public:
+        IndexError(std::string what_arg);
+};
+
+class ValueError: public Exception
+{
+    public:
+        ValueError(std::string what_arg);
+};
+
+class TypeError: public Exception
+{
+    public:
+       TypeError(std::string what_arg);
+};
+
+#endif
