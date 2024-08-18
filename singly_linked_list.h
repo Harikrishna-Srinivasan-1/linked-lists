736a2c46a9d3cce50e7df08c595b884c8a4b8e42
Harikrishna Srinivasan <176735075+Harikrishna-Srinivasan-1@users.noreply.github.com>
2024-08-18 13:52:36 +0530

    Update singly_linked_list.h
    Added operators, copy and reverse methods
    
    
    Signed-off-by: Harikrishna Srinivasan <176735075+Harikrishna-Srinivasan-1@users.noreply.github.com>

--- singly_linked_list.h
+++ singly_linked_list.h
@@ -9,6 +9,7 @@ class singly_linked_list: public linkedlist<A>
 {
     public:
         singly_linked_list();
+        singly_linked_list(const singly_linked_list<A> &obj);
 
         void prepend(const A &value);
         void append(const A &value);
@@ -23,21 +24,39 @@ class singly_linked_list: public linkedlist<A>
         size_t find(const A &value) const;
         size_t count(const A &value) const;
 
+        void reverse();
+        // void sort();
+
+        singly_linked_list<A> &copy(const linkedlist<A> &obj);
+
         A &operator[](const int64_t &index);
-        linkedlist<A> &operator+(const linkedlist<A> &obj);
-        linkedlist<A> &operator=(const linkedlist<A> &obj);
-        // linkedlist<A> &operator*(const linkedlist<A> &obj);
-        // bool operator>(const linkedlist<A> &obj) const;
-        // bool operator<(const linkedlist<A> &obj) const;
-        // bool operator>=(const linkedlist<A> &obj) const;
-        // bool operator<=(const linkedlist<A> &obj) const;
-        // bool operator==(const linkedlist<A> &obj) const;
+        singly_linked_list<A> operator+(const linkedlist<A> &obj);
+        singly_linked_list<A> &operator=(const singly_linked_list<A> &obj);
+        singly_linked_list<A> operator*(const size_t &times);
+        bool operator>(const linkedlist<A> &obj) const;
+        bool operator<(const linkedlist<A> &obj) const;
+        bool operator>=(const linkedlist<A> &obj) const;
+        bool operator<=(const linkedlist<A> &obj) const;
+        bool operator==(const linkedlist<A> &obj) const;
+        bool operator!=(const linkedlist<A> &obj) const;
 
         void show();
 
         void clear();
 
+        template <typename B>
+        friend singly_linked_list<B> operator*(const size_t &times, const singly_linked_list<B> &obj);
+        
+        template <typename B>
+        friend std::ostream &operator<<(std::ostream &out, singly_linked_list<B> &obj);
+
         ~singly_linked_list();
 };
+
+template <typename A>
+singly_linked_list<A> operator*(const size_t &times, singly_linked_list<A> &obj);
+
+template <typename A>
+std::ostream &operator<<(std::ostream &out, singly_linked_list<A> &obj);
 
 #endif
