2eb1f94f245a27cd3aae30c1691eb6260020eb89
Harikrishna Srinivasan <176735075+Harikrishna-Srinivasan-1@users.noreply.github.com>
2024-08-18 13:50:25 +0530

    Update All cleared in linkedlist.cpp
    Signed-off-by: Harikrishna Srinivasan <176735075+Harikrishna-Srinivasan-1@users.noreply.github.com>

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,48 +1,72 @@
-#include "linkedlist.h"
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
 
+#include <exception>
+#include <iostream>
+
+const size_t NOT_FOUND = -1;
 
 template <typename A>
-linkedlist<A>::linkedlist():
-head(nullptr), length(0), tail(nullptr) {}
-
-template <typename A>
-typename linkedlist<A>::node *linkedlist<A>::gethead(const linkedlist<A> &obj) 
+class linkedlist
 {
-    return obj.head;
-}
+    protected:
+        typedef struct node
+        {
+            A value;
+            struct node *next;
+        } node;
+
+        node *head, *tail;
+        size_t length;
+        
+        static node *gethead(const linkedlist<A> &obj);
+        static node *gettail(const linkedlist<A> &obj);
+    public:
+        linkedlist();
+
+        virtual void prepend(const A &value) = 0;
+        virtual void append(const A &value) = 0;
+        virtual void insert(const A &value, const int64_t &index) = 0;
+        virtual void insert_inorder(const A &value) = 0;
+        virtual void extend(const linkedlist<A> &obj) = 0;
+
+        virtual void remove(const A &value) = 0;
+        virtual A pop(const int64_t &index) = 0;
+        virtual A pop() = 0;
+
+        virtual size_t size() const;
+
+        virtual size_t find(const A &value) const = 0;
+        virtual size_t count(const A &value) const = 0;
+
+        virtual bool isempty() const;
+        
+        virtual void reverse() = 0;
+        // virtual void sort() = 0;   
+
+        // virtual linkedlist<A> &copy(const linkedlist<A> &obj) = 0;
+
+        virtual A &operator[](const int64_t &index) = 0;
+        // virtual linkedlist<A> &operator=(const linkedlist<A> &obj) = 0;
+        // virtual linkedlist<A> &operator+(const linkedlist<A> &obj) = 0;
+        // virtual linkedlist<A> &operator*(const size_t &times) = 0;
+        virtual bool operator>(const linkedlist<A> &obj) const = 0;
+        virtual bool operator<(const linkedlist<A> &obj) const = 0;
+        virtual bool operator>=(const linkedlist<A> &obj) const = 0;
+        virtual bool operator<=(const linkedlist<A> &obj) const = 0;
+        virtual bool operator==(const linkedlist<A> &obj) const = 0;
+        virtual bool operator!=(const linkedlist<A> &obj) const = 0;
+
+        virtual void clear() = 0;
+
+        template <typename B>
+        friend size_t len(const linkedlist<B> &obj);
+        
+
+        virtual ~linkedlist();
+};
 
 template <typename A>
-typename linkedlist<A>::node *linkedlist<A>::gettail(const linkedlist<A> &obj) 
-{
-    return obj.tail;
-}
+size_t len(const linkedlist<A> &obj);
 
-template <typename A>
-size_t linkedlist<A>::size() const
-{
-    return this->length;
-}
-
-template <typename A>
-bool linkedlist<A>::isempty() const
-{
-    return (this->head == nullptr);
-}
-
-template <typename A>
-linkedlist<A>::~linkedlist() {}
-
-template <typename A>
-size_t len(const linkedlist<A> &obj)
-{
-    size_t size = 0;
-    typename linkedlist<A>::node *ptr = obj.head;
-
-    while (ptr != nullptr)
-    {
-        size++;
-        ptr = ptr->next;
-    }
-
-    return size;
-}
+#endif
